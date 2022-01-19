7fac143027cd1f1a749e4cf59b773ae5b4c827da
OrestisStefanou <orestaras1997@outlook.com>
2022-01-19 11:02:43 +0200

    Procfile bug fixed

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css", dbc.themes
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
+server = app.server
+
 main_page_callbacks.get_callbacks(app)
 
 app.layout = main_page_layout.get_layout()
